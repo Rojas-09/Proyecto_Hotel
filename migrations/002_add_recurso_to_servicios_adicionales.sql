3a3320f618ea62a0c83b213fbeed53228e4de04b
Rojas-09 <juan.rojas7@utp.edu.co>
2026-06-24 00:21:45 -0500

    chore: remove old migrations/ folder (superseded by alembic/)

--- migrations/002_add_recurso_to_servicios_adicionales.sql
+++
@@ -1,5 +0,0 @@
--- Migration 002: Add recurso column to servicios_adicionales
--- RF-11: Validar traslapes por recurso (sala/masajista)
-
-ALTER TABLE servicios_adicionales
-ADD COLUMN recurso VARCHAR(100);
